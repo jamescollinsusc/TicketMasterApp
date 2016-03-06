0e7c106c198b5fbdee3f6796df8227cdd855693d
1512sps <jcollins@sps.edu>
2016-03-06 11:10:19 -0800

    trying again

--- app/src/main/java/com/example/trey/attempt2/MainActivity.java
+++ app/src/main/java/com/example/trey/attempt2/MainActivity.java
@@ -11,7 +11,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import static com.example.trey.attempt2.TMapi.doInBackground;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,7 +31,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        timmy = doInBackground();
+        try {
+            timmy = getText();
+        } catch (Exception e) {
+            timmy = "nooooo";
+        }
         mjimmy = (TextView) findViewById(R.id.JimmyTheShit);
         mUsername = (EditText) findViewById(R.id.username);
         mPassword = (EditText) findViewById(R.id.password);
@@ -69,4 +77,24 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    public static String getText() throws Exception {
+
+        URL website = new URL("https://app.ticketmaster.com/discovery/v1/events.json?postalCode=90007&apikey=pGNwjKe1ggesYToqvem4wg7DgvyuInuV");
+        URLConnection connection = website.openConnection();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                        connection.getInputStream()));
+
+        StringBuilder response = new StringBuilder();
+        String inputLine;
+
+        while ((inputLine = in.readLine()) != null)
+            response.append(inputLine);
+
+        in.close();
+
+        return response.toString();
+    }
+
+
 }
